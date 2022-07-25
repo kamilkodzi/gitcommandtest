d52587399376f643182d1b5f95a8d89cb080dc9b
Kamil Adamski <kadamski@thefarm51.com>
2022-07-25 10:57:35 +0200

    sad

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Git Test commands")
+// const { exec } = require('node:child_process');
+// console.log("Git Test commands")
+// exec("git fetch",(error, stdout, stderr) => {
+//     if (error) {
+//         console.error(`exec error: ${error}`);
+//         return;
+//       }
+//       console.log(`stdout: ${stdout}`);
+//       console.error(`stderr: ${stderr}`);
+// })
+
+const util = require('node:util');
+const exec = util.promisify(require('node:child_process').exec);
+
+async function lsExample() {
+  const statusA = await exec('git fetch');
+//   console.log('stdout:', stdout);
+//   console.error('stderr:', stderr);
+console.log(statusA)
+}
+lsExample();
