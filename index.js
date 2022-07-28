00398321dbc69c518b45f859a931d7ebcfaa0372
kadamski <kamil.adamski@thefarm51.com>
2022-07-28 13:37:46 +0200

    fix

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
-// const { exec } = require('node:child_process');
-// console.log("Git Test commands")
-// exec("git fetch",(error, stdout, stderr) => {
-//     if (error) {
-//         console.error(`exec error: ${error}`);
-//         return;
-//       }
-//       console.log(`stdout: ${stdout}`);
-//       console.error(`stderr: ${stderr}`);
-// })
-
-const util = require('node:util');
-const exec = util.promisify(require('node:child_process').exec);
+const util = require("node:util");
+const exec = util.promisify(require("node:child_process").exec);
 
 async function lsExample() {
-    try {
-        const statusA = await exec('git fetch');
-        // const rawForm = await exec("git diff --stat --raw origin/main")
-        const {stdout,stderr} = await exec("git status")
+  const regex = new RegExp("^[A-Z]");
+  const { stdout, stderr } = await exec("git status");
+  const splitedString = stdout.split("\n");
 
-        console.log("Is branch up to date: ",!stdout.includes("Your branch is behind"))
-        // console.log("DIF START: ",rawForm)
-        // console.log("DIF ENDS: ")
-    } catch (error) {
-        console.log("Error to :" ,error,"Koniec Errora")
-    }
-  
-//   console.log('stdout:', stdout);
-//   console.error('stderr:', stderr);
+  const reducedArray = splitedString.reduce((result, line,index) => {
+    if (line.includes("Your branch is")) return {...result,remoteDetails:line}
+    if (line.includes("Changes")) return {...result,changesDetails:{...result.changesDetails,message:(result.changesDetails.message+line)}}
+    if (line.includes("modified")) return  {...result,changesDetails:{...result.changesDetails,files:[...result.changesDetails.files, line]}}
+    return {...result};
+  },{remoteDetails:null,changesDetails:{
+    message:"",
+    files:[]
+  }});
 
+  const filteredArray = splitedString.filter((line) => line.match(regex));
+  console.log(filteredArray);
+  console.log(reducedArray)
 }
 lsExample();
