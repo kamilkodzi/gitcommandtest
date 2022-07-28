ad2d093690086d9262a67622a0feb5a07e5fc0f6
kadamski <kamil.adamski@thefarm51.com>
2022-07-28 13:46:56 +0200

    fix

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ const util = require("node:util");
 const exec = util.promisify(require("node:child_process").exec);
 
 async function lsExample() {
-  const regex = new RegExp("^[A-Z]");
   const { stdout, stderr } = await exec("git status");
   const splitedString = stdout.split("\n");
 
   const reducedArray = splitedString.reduce((result, line,index) => {
     if (line.includes("Your branch is")) return {...result,remoteDetails:line}
     if (line.includes("Changes")) return {...result,changesDetails:{...result.changesDetails,message:(result.changesDetails.message+line)}}
-    if (line.includes("modified")) return  {...result,changesDetails:{...result.changesDetails,files:[...result.changesDetails.files, line]}}
+    if (line.includes("modified")) return  {...result,changesDetails:{...result.changesDetails,files:[...result.changesDetails.files, line.split(" ").pop()]}}
     return {...result};
   },{remoteDetails:null,changesDetails:{
-    message:null,
+    message:"",
     files:[]
   }});
-
-  const filteredArray = splitedString.filter((line) => line.match(regex));
-  console.log(filteredArray);
-  console.log(reducedArray)
+console.log(reducedArray)
 }
 lsExample();
