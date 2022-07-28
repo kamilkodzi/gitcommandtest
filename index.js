64432183025355e6fed0d983ed37a2f6fa487e00
kadamski <kamil.adamski@thefarm51.com>
2022-07-28 13:55:09 +0200

    fix

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ async function lsExample() {
   const reducedArray = splitedString.reduce((result, line,index) => {
     if (line.includes("Your branch is")) return {...result,remoteDetails:line}
     if (line.includes("Changes")) return {...result,changesDetails:{...result.changesDetails,message:(result.changesDetails.message,line)}}
+    if (line.includes("nothing to commit, working tree clean")) return {...result,changesDetails:{...result.changesDetails,message:(result.changesDetails.message,line)}}
     if (line.includes("modified")) return  {...result,changesDetails:{...result.changesDetails,files:[...result.changesDetails.files, line.split(" ").pop()]}}
     return {...result};
   },{remoteDetails:null,changesDetails:{
