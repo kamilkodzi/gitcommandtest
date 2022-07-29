c614cc77a948ed5900968408333700c6e30ed192
kamilkodzi <bizkamiladamski@gmail.com>
2022-07-29 10:14:44 +0200

    fix

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const exec = util.promisify(require("node:child_process").exec);
 
 async function lsExample() {
   const { stdout, stderr } = await exec("git status");
+  const porcelain=await exec("git status -porcelain");
   const splitedString = stdout.split("\n");
 
   const reducedArray = splitedString.reduce((result, line,index) => {
     if (line.includes("Your branch is")) return {...result,remoteDetails:line}
-    if (line.includes("Changes")) return {...result,changesDetails:{...result.changesDetails,message:(result.changesDetails.message,line)}}
+    if (line.includes("Changes")) return {...result,changesDetails:{...result.changesDetails,message:(result.changesDetails.message+line)}}
+    if (line.includes("Untracked files")) return {...result,changesDetails:{...result.changesDetails,message:(result.changesDetails.message+line)}}
     if (line.includes("nothing to commit, working tree clean")) return {...result,changesDetails:{...result.changesDetails,message:(result.changesDetails.message,line)}}
     if (line.includes("modified")) return  {...result,changesDetails:{...result.changesDetails,files:[...result.changesDetails.files, line.split(" ").pop()]}}
     return {...result};
-  },{remoteDetails:null,changesDetails:{
-    message:null,
+  },{remoteDetails:"",changesDetails:{
+    message:"",
     files:[]
   }});
 console.log(reducedArray)
