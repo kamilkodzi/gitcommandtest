518708bb9ba36fdd2d39063c7b6dbbdebf623928
kadamski <kamil.adamski@thefarm51.com>
2022-07-29 11:31:23 +0200

    fix

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const exec = util.promisify(require("node:child_process").exec);
 async function lsExample() {
   const { stdout, stderr } = await exec("git status");
   const splitedString = stdout.split("\n");
+  const test = undefined
 
+  if (test) console.log("hello!")
   const reducedArray = splitedString.reduce((result, line,index) => {
     if (line.includes("Your branch is")) return {...result,remoteDetails:line}
     if (line.includes("Changes")) return {...result,changesDetails:{...result.changesDetails,message:(result.changesDetails.message,line)}}
